5ab058c69812c6860cd00a937daf6feffb526f83
Gonshz <1031gonshi@gmail.com>
2021-02-22 18:50:54 +0900

    404ページ作成

---
+++ src/pages/404.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
+
+function NotFound() {
+    return (
+        <Layout>
+            <h1>page not found</h1>
+            <p><Link to="/" >Head home</Link></p>
+        </Layout>
+    )
+}
+
+export default NotFound
